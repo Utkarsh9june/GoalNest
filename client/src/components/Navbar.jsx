0518cc8b218d506336cf174f437c0115e441cac5
Utkarsh9june <utkarshgoyal648@gmail.com>
2025-10-02 18:02:30 +0530

    Navbar Icon Changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,11 +6,11 @@ import { useNavigate } from "react-router-dom";
 const Navbar = () => {
   const navigate = useNavigate();
   return (
-    <nav className="flex items-center text-center justify-between px-4 py-2 z-10">
+    <nav className="flex items-center text-center justify-between px-4 py-2 z-10 bg-blue-100">
       <img
         src={logo}
         alt="GoalNest"
-        className="cursor-pointer h-9 m-1"
+        className="cursor-pointer h-9 m-1 rounded-xl"
         onClick={() => navigate("/")}
       />
       <h1 className="text-4xl font-bold pl-52">
